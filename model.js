aedb61785e96dd51ec112f6e0e5f725b889cd2e2
MD MOQADDAS <sgn9556@gmail.com>
2023-04-09 00:17:16 +0530

    two collection of schema working fine.

--- model.js
+++ model.js
@@ -4,7 +4,7 @@ mongoose.connect("mongodb://localhost:27017/cgig").then(
     ()=>{console.log("Connected with the database....")}
 ).catch(()=>{console.log("Connection fail  with database...")})
 
-const schema ={
+const userschema ={
     name: {
         type: String,
         required: [true, "Name can't be optional"]
@@ -19,6 +19,21 @@ const schema ={
     }
 }
 
-const postSchema = mongoose.Schema(schema);
+const postschema = {
+    title:{
+        type: String,
+        required: [true, "title is mandotry"]
+    },
+    description: {
+        type: String,
+        require: [true, "description is mandotry"]
+    }
+}
 
-module.exports = mongoose.model('User', postSchema);
+const userSchema = new mongoose.Schema(userschema);
+const postSchema = new mongoose.Schema(postschema);
+
+const User = mongoose.model('User', userSchema);
+const Post = mongoose.model('Post', postSchema);
+
+module.exports = { User, Post };
--- server.js
+++ server.js
@@ -1,10 +1,12 @@
-const User = require('./model');
+const { User, Post } = require('./model');
 const express = require('express');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const router = require('express').Router();
 const app = express();
 const bodyParser = require("body-parser");
+const multer = require('multer');
+const path = require('path');
 const port = 3001;
 
 //it'll only allow the localhost:3000
@@ -55,7 +57,7 @@ router.post("/login", async (req, res) => {
     // create jwt token
     const token = jwt.sign({ email: user.email, userId: user._id }, secret, { expiresIn: '1h' });
 
-    res.status(200).json({ token, expiresIn: 3600 ,  userId: user._id });
+    res.status(200).json({ token, expiresIn: 3600, userId: user._id });
   } catch (error) {
     console.error(error);
     res.status(500).send("Server error"); // handle internal server error
@@ -119,169 +121,92 @@ function authorize(req, res, next) {
 router.post('/logout', authorize, (req, res) => {
   res.clearCookie('authToken')
   res.status(200).json({ message: 'Logout successful' });
-  
+
 })
 
-// function authorize(req, res, next) {
-//   try {
-//     const token = req.headers.authorization.split(" ")[1];
-//     const decoded = jwt.verify(token, secret);
-//     req.userData = decoded;
-//     next();
-//   } catch (error) {
-//     return res.status(401).json({ message: 'Auth failed' });
-//   }
-// }
 
 
+// Create a new post
+router.post('/posts', (req, res) => {
+  const post = new Post({
+    title: req.body.title,
+    description: req.body.description,
+  });
+  post.save((error, post) => {
+    if (error) {
+      res.status(500).send(error);
+    } else {
+      res.status(201).send("Successfully saved the post");
+    }
+  });
+});
+
+//Get all the posts
+
+router.get('/posts', (req, res) => {
+  Post.find({}, (error, posts) => {
+    if (error) {
+      res.status(500).send(error);
+    } else {
+      res.send(posts);
+    }
+  });
+});
 
 
-// const User = require('./model');
-// const express = require('express');
-// const bcrypt = require('bcrypt')
-// const router = require('express').Router();
-// const app = express();
-// const bodyParser = require("body-parser")
-// const port = 3001;
+// Get a single post by ID
+router.get('/posts/:id', (req, res) => {
+  Post.findById(req.params.id, (error, post) => {
+    if (error) {
+      res.status(500).send(error);
+    } else {
+      res.send(post);
+    }
+  });
+});
+
+// Update a post by ID
+router.put('/posts/:id', (req, res) => {
+  Post.findByIdAndUpdate(req.params.id, { title: req.body.title, description: req.body.description }, (error, post) => {
+    if (error) {
+      res.status(500).send(error);
+    } else {
+      res.send(post);
+    }
+  });
+});
+
+// Delete a post by ID
+router.delete('/posts/:id', (req, res) => {
+  Post.findByIdAndDelete(req.params.id, (error, post) => {
+    if (error) {
+      res.status(500).send(error);
+    } else {
+      res.send(post);
+    }
+  });
+});
 
 
-// //it'll only allow the localhost:3000
-// app.use((req, res, next) => {
-//   res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//   next();
-// })
-// app.use(express.urlencoded({ extended: true }))
-// app.use(bodyParser.json())
-// app.use('/api', router);
-// //all the api listening at /api/posts
+//object storeage in local disk
+// Set the storage engine for multer
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/')
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
+  }
+});
 
-// //secret for the session
+// Initialize multer middleware
+const upload = multer({ storage: storage });
 
-// app.get('/', (req, res) => {
-//   res.send('API Listening on ip:/api/posts');
-// });
+router.post('/notes', upload.single('file'), (req, res) => {
 
-// //Start the server and listen on specified port
-// app.listen(port, () => {
-//   console.log(`Server listening at *:${port}`);
-// });
-
-
-
-
-// router.post("/login", async (req, res) => {
-//   try {
-//     const { email, password } = req.body;
-//     //console.log(email + " " + password)
-
-//     const user = await User.findOne({ email });
-
-//     if (!user) {
-//       return res.send(false); // user not found
-//     }
-
-//     const validPassword = await bcrypt.compare(password, user.password);
-
-//     if (validPassword) {
-     
-     
-//       res.send(true);
-
-//     } else {
-//       res.send(false); // incorrect password
-//     }
-//   } catch (error) {
-//     console.error(error);
-//     res.status(500).send("Server error"); // handle internal server error
-//   }
-// })
-
-
-
-// router.post("/signup", async (req, res) => {
-//   const { name, email, password } = req.body;
-//   // console.log("Singup : " + name + " " + email + " " + password);
-
-//   const hashPassword = await bcrypt.hash(password, 12);
-//   const user = new User({
-//     name: name,
-//     email: email,
-//     password: hashPassword
-//   })
-
-//   try {
-//     await user.save();
-//     res.send(true)
-//   } catch (error) {
-//     console.error(error);
-//     res.send(false);
-//   }
-
-// })
-
-
-
-
-
-
-
-// // Create a new post
-// router.post('/posts', (req, res) => {
-//   const post = new Post({
-//     title: req.body.title,
-//     content: req.body.content,
-//   });
-//   post.save((error, post) => {
-//     if (error) {
-//       res.status(500).send(error);
-//     } else {
-//       res.status(201).send("Successfully saved the code");
-//     }
-//   });
-// });
-
-// // Get all posts
-// router.get('/posts', (req, res) => {
-//   Post.find((error, posts) => {
-//     if (error) {
-//       res.status(500).send(error);
-//     } else {
-//       res.send(posts);
-//     }
-//   });
-// });
-
-// // Get a single post by ID
-// router.get('/posts/:id', (req, res) => {
-//   Post.findById(req.params.id, (error, post) => {
-//     if (error) {
-//       res.status(500).send(error);
-//     } else {
-//       res.send(post);
-//     }
-//   });
-// });
-
-// // Update a post by ID
-// router.put('/posts/:id', (req, res) => {
-//   Post.findByIdAndUpdate(req.params.id, { title: req.body.title, content: req.body.content }, (error, post) => {
-//     if (error) {
-//       res.status(500).send(error);
-//     } else {
-//       res.send(post);
-//     }
-//   });
-// });
-
-// // Delete a post by ID
-// router.delete('/posts/:id', (req, res) => {
-//   Post.findByIdAndDelete(req.params.id, (error, post) => {
-//     if (error) {
-//       res.status(500).send(error);
-//     } else {
-//       res.send(post);
-//     }
-//   });
-// });
-
+  const filePath = req.file.path;
+  // Save the file path and other details to the database or perform any other actions
+  // In this example, we're just sending back the saved file path and details
+  // res.json({ filePath, title, description });
+  res.status(200).send("file saved")
+});
